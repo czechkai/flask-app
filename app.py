2cdc029196d1b9552e7104ca3a1266dc475c6ff0
Your Name <czechkai>
2025-11-15 16:18:46 +0800

    updated

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-from flask import Flask 
+# from flask import Flask 
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'Hello, Cloud! This is a PaaS Deployment.'
+
+# if __name__ == '__main__':
+#     app.run()
+
+import os
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +18,5 @@ def hello():
     return 'Hello, Cloud! This is a PaaS Deployment.'
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
